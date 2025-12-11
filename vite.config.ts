f9061f0383aea5432a1b9b48618481ea675239c9
ansango <anibalsantosgo@gmail.com>
2025-12-11 18:45:10 +0100

    feat: add coverage support with Vitest and update test scripts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,10 @@ import { sveltekit } from '@sveltejs/kit/vite';
 export default defineConfig({
 	plugins: [tailwindcss(), sveltekit()],
 	test: {
+		coverage: {
+			provider: 'v8',
+			include: ['src/**/*.{ts,js,svelte}'],
+		},
 		expect: { requireAssertions: true },
 		projects: [
 			{
